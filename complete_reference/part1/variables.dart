8ddb4c22be867cdc87561f1f8e22b1768ff4709f
Maxtoshie <maaxwelokoth@gmail.com>
2025-10-31 04:08:14 +0300

    Initializing variables

---
+++ complete_reference/part1/variables.dart
@@ -0,0 +1,11 @@
+// Automatic type inference
+var value = 18;
+var myName = "Maxtoshie";
+
+// Explicit typeing 
+int value1 = 18;
+String myName1 = "Maxtoshie";
+
+// Dynamic typing
+dynamic value2 = 18;
+dynamic myName2 = "Maxtoshie";
